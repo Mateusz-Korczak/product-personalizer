6fa5b19e1d408ee75f4a8c6f59db6a585054ec7f
Mateusz Korczak <korczak.programowanie@gmail.com>
2022-01-03 16:49:25 +0100

    fixed autoload properly product options

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,46 +1,75 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import propTypes from 'prop-types';
+import { useState } from 'react';
 
-const Product = props => {
+const Product = ({ title, basePrice, colors, sizes, name }) => {
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+  console.log(currentColor);
+  console.log(currentSize);
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
-        <img 
+        <img
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`}
+        />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>{basePrice}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              <li>
+                <button type='button' className={styles.active}>
+                  S
+                </button>
+              </li>
+              <li>
+                <button type='button'>M</button>
+              </li>
+              <li>
+                <button type='button'>L</button>
+              </li>
+              <li>
+                <button type='button'>XL</button>
+              </li>
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              <li>
+                <button
+                  type='button'
+                  className={clsx(styles.colorBlack, styles.active)}
+                />
+              </li>
+              <li>
+                <button type='button' className={clsx(styles.colorRed)} />
+              </li>
+              <li>
+                <button type='button' className={clsx(styles.colorWhite)} />
+              </li>
             </ul>
           </div>
           <Button className={styles.button}>
-            <span className="fa fa-shopping-cart" />
+            <span className='fa fa-shopping-cart' />
           </Button>
         </form>
       </div>
     </article>
-  )
+  );
 };
-
+Product.propTypes = {
+  title: propTypes.string.isRequired,
+};
 export default Product;
